980e5cadcfb85c6b9724f95966908ff74a68113b
ThomasMAC <tuds_wilkinson@hotmail.com>
2014-12-08 09:21:13 +0100

    Cleanup stray files

--- db/schema.rb
+++ db/schema.rb
@@ -59,18 +59,4 @@ ActiveRecord::Schema.define(version: 20140818093150) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
-  create_table "votes", force: true do |t|
-    t.boolean  "vote",          default: false, null: false
-    t.integer  "voteable_id",                   null: false
-    t.string   "voteable_type",                 null: false
-    t.integer  "voter_id"
-    t.string   "voter_type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "votes", ["voteable_id", "voteable_type"], name: "index_votes_on_voteable_id_and_voteable_type"
-  add_index "votes", ["voter_id", "voter_type", "voteable_id", "voteable_type"], name: "fk_one_vote_per_user_per_entity", unique: true
-  add_index "votes", ["voter_id", "voter_type"], name: "index_votes_on_voter_id_and_voter_type"
-
 end
